50e21448ce1046259ca3e168823985c139645352
NataliiaKostiuk <128825690+NataliiaKostiuk@users.noreply.github.com>
2023-08-15 15:30:20 +0100

    remade task 2

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,58 +1,29 @@
 import throttle from "lodash.throttle";
 const throttle = require('lodash.throttle');
+
+const form = document.querySelector('.feedback-form');
+form.addEventListener('input', throttle(handlerInput), 500);
+form.addEventListener('submit', handlerClear);
  
 const KEY_STORAGE = "feedback-form-state";
-const formValue = {
-    email: '',
-    message: '', 
-}
-const form = document.querySelector('.feedback-form');
-storageValueToInput();
 
+let formValue = JSON.parse(localStorage.getItem(KEY_STORAGE)) ?? {};
+const { email, message } = form.elements;
+email.value = formValue.email ?? "";
+message.value = formValue.message ?? "";
 
-form.addEventListener('input',
-    throttle(evt => {
-        if (evt.target.name === 'email') {
-        KEY_STORAGE.email = evt.target.value;
-        formValue.email = form.email.value;  
-    }
-        if (evt.target.name === 'message') {
-         KEY_STORAGE.message = evt.target.value;
-         formValue.message = form.message.value;
-     } 
-        localStorage.setItem(KEY_STORAGE, JSON.stringify(formValue))
- }
-     , 500   ))
-
-
-function storageValueToInput() {
-  if (
-    localStorage.length > 0 &&
-    Object.keys(localStorage).includes(KEY_STORAGE)
-  ) { 
-      form.email.value = JSON.parse(localStorage.getItem(KEY_STORAGE)).email;
-      form.message.value = JSON.parse(localStorage.getItem(KEY_STORAGE)).message;
-      formValue.email = JSON.parse(localStorage.getItem(KEY_STORAGE)).email;
-      formValue.message = JSON.parse(localStorage.getItem(KEY_STORAGE)).message;   
-  }
+function handlerInput(evt) {
+    formValue[evt.target.name] = evt.target.value;
+    localStorage.setItem(KEY_STORAGE, JSON.stringify(formValue));
     
 }
 
 
-form.addEventListener('submit', handlerClear);
 function handlerClear(evt) {
-    evt.preventDefault();
-    if (localStorage.length > 0 &&
-    Object.keys(localStorage).includes(KEY_STORAGE)) {
-console.log(JSON.parse(localStorage.getItem(KEY_STORAGE)))
-    }
-    formValue.email = '';
-    formValue.message = '';
-    form.email.value = '';
-    form.message.value = '';
-     localStorage.removeItem(KEY_STORAGE);
+     evt.preventDefault();
+    console.log(formValue);
+    formValue = {};
+     form.reset();
+    localStorage.removeItem(KEY_STORAGE);
     
 }
-
-
-
